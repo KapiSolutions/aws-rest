275982caa4a9967d41301bcfdfbc41efd7576374
KapiSolutions <kuba.kapek@gmail.com>
2022-01-20 16:43:22 +0100

    changes

--- src/handlers/get-by-id.js
+++ src/handlers/get-by-id.js
@@ -6,10 +6,10 @@ const dynamodb = require('aws-sdk/clients/dynamodb');
 const docClient = new dynamodb.DocumentClient();
 
 // Get the DynamoDB table name from environment variables
-const tableName = process.env.SAMPLE_TABLE;
+const tableName = process.env.USERS_TABLE;
 
 /**
- * A simple example includes a HTTP get method to get one item by id from a DynamoDB table.
+ * A simple example includes a HTTP get method to get one item by user_id from a DynamoDB table.
  */
 exports.getByIdHandler = async (event) => {
     const { httpMethod, path, pathParameters } = event;
@@ -20,14 +20,14 @@ exports.getByIdHandler = async (event) => {
     // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
     console.log('received:', JSON.stringify(event));
 
-    // Get id from pathParameters from APIGateway because of `/{id}` at template.yml
-    const { id } = pathParameters;
+    // Get id from pathParameters from APIGateway because of `/{user_id}` at template.yml
+    const { user_id } = pathParameters;
 
     // Get the item from the table
     // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#get-property
     const params = {
         TableName: tableName,
-        Key: { id },
+        Key: { user_id },
     };
     const { Item } = await docClient.get(params).promise();
 
