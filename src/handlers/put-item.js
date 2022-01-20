c4dca6f03eec59046eeb74c8ba3e2acf0c61576b
KapiSolutions <kuba.kapek@gmail.com>
2022-01-20 19:31:43 +0100

    changes

--- src/handlers/put-item.js
+++ src/handlers/put-item.js
@@ -2,40 +2,42 @@
 
 // Create a DocumentClient that represents the query to add an item
 const dynamodb = require('aws-sdk/clients/dynamodb');
-
 const docClient = new dynamodb.DocumentClient();
 
 // Get the DynamoDB table name from environment variables
 const tableName = process.env.USERS_TABLE;
 
-/**
- * A simple example includes a HTTP post method to add one item to a DynamoDB table.
- */
+// Include uuid lib to generate random user_id uuid
+const { v4: uuidv4 } = require('uuid');
+
+// HTTP post method to add one item to a DynamoDB table.
 exports.putItemHandler = async (event) => {
     const { body, httpMethod, path } = event;
     if (httpMethod !== 'POST') {
         throw new Error(`postMethod only accepts POST method, you tried: ${httpMethod} method.`);
     }
-    // All log statements are written to CloudWatch by default. For more information, see
-    // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
-    console.log('received:', JSON.stringify(event));
 
-    // Get id and name from the body of the request
-    const { user_id, name } = JSON.parse(body);
+    //console.log('received:', JSON.stringify(event));
 
-    // Creates a new item, or replaces an old item with a new item
-    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+    // Get name and age from the body of the request
+    const { name, age } = JSON.parse(body);
+    // Generate uuid for the new user_id
+    var user_id = uuidv4();
+
+    // Create a new user 
     const params = {
         TableName: tableName,
-        Item: { user_id, name },
+        Item: { user_id, name, age },
     };
+    
     await docClient.put(params).promise();
 
+    //Construct the response
     const response = {
         statusCode: 200,
-        body,
+        body: "user_id: " + user_id,
     };
 
-    console.log(`response from: ${path} statusCode: ${response.statusCode} body: ${response.body}`);
+    //console.log(`response from: ${path} statusCode: ${response.statusCode} body: ${response.body}`);
     return response;
 };
