2ecc6ec18ff72c76b7ccf66fe9eba1ae43a20b35
KapiSolutions <kuba.kapek@gmail.com>
2022-01-20 22:59:38 +0100

    Update get-by-id.js

--- src/handlers/get-by-id.js
+++ src/handlers/get-by-id.js
@@ -77,7 +77,7 @@ exports.getByIdHandler = async (event) => {
         TableName: tableName,
         //IndexName: 'some-index',
         KeyConditionExpression: '#name = :value',
-        ExpressionAttributeValues: { ':value': user_id },
+        ExpressionAttributeValues: { ':value': 'b4d2e33e-4692-4a13-b6a7-e45a51a4f9d8' },
         ExpressionAttributeNames: { '#name': 'user_id' }
       }
 
