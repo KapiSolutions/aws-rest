f55775a757f9db545e46a9313eb7af20aee16960
KapiSolutions <kuba.kapek@gmail.com>
2022-01-20 18:10:21 +0100

    origin

--- src/handlers/put-item.js
+++ src/handlers/put-item.js
@@ -6,7 +6,7 @@ const dynamodb = require('aws-sdk/clients/dynamodb');
 const docClient = new dynamodb.DocumentClient();
 
 // Get the DynamoDB table name from environment variables
-const tableName = process.env.USERS_TABLE;
+const tableName = process.env.SAMPLE_TABLE;
 
 /**
  * A simple example includes a HTTP post method to add one item to a DynamoDB table.
@@ -21,13 +21,13 @@ exports.putItemHandler = async (event) => {
     console.log('received:', JSON.stringify(event));
 
     // Get id and name from the body of the request
-    const { user_id, name } = JSON.parse(body);
+    const { id, name } = JSON.parse(body);
 
     // Creates a new item, or replaces an old item with a new item
     // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
     const params = {
         TableName: tableName,
-        Item: { user_id, name },
+        Item: { id, name },
     };
     await docClient.put(params).promise();
 
